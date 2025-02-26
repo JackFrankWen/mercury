a08b35008cfedef5da9a55c7e41c341d99cec53d
baiqiu.wsn <baiqiu.wsn@raycloud.com>
2025-02-26 11:43:05 +0800

    ui

--- src/UI/page/app.tsx
+++ src/UI/page/app.tsx
@@ -1,81 +1,105 @@
-import React   from "react";
-import {Layout, Menu, Tabs} from "antd";
-import KeepAlive, {AliveScope} from "react-activation";
+import React from "react";
+import { Layout, ConfigProvider, Menu } from "antd";
+import KeepAlive, { AliveScope } from "react-activation";
 import { useNavigate } from "react-router-dom";
 import Home from "./home";
 import Accounting from "./accounting";
 import Setting from "./setting";
 import Upload from "./upload";
 
+
 import {
     AccountBookOutlined,
-SettingOutlined,
-FormOutlined,
+    SettingOutlined,
+    FormOutlined,
     CloudUploadOutlined,
     HomeOutlined,
+    ReadOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 type MenuItem = Required<MenuProps>['items'][number];
 
 
+
 const items: MenuItem[] = [
-    { key: 'home', icon: <HomeOutlined />, label: '首页' },
-    { key: 'accounting', icon: <FormOutlined /> , label: '记账' },
+    { key: 'home', icon: <ReadOutlined  />, label: '首页' },
+    { key: 'accounting', icon: <FormOutlined />, label: '记账' },
     { key: 'upload', icon: <CloudUploadOutlined />, label: '导入' },
     { key: 'setting', icon: <SettingOutlined />, label: '设置' },
 ];
 const { Header, Content, Footer, Sider } = Layout;
 
+
 function App(props: any): JSX.Element {
     const [activeKey, setActiveKey] = React.useState('home');
     return (
-        <Layout
-            style={{
-                height: '100vh',
-            }}>
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "#eccf10",
+                    colorInfo: "#eccf10"
+                },
+                components: {
+                    Menu: {
+                        iconSize: 16,
+                    },
+                    Layout: {
+                    },
 
-            <Sider
-                collapsed={true}
-                className="mercury-sider"
-            >
-                <Menu
+                },
+
+            }}
+        >
+            <Layout
+                style={{
+                    height: '100vh',
+                }}>
+
+                <Sider
+                    theme="light"
+                    collapsed={true}
+                    className="mercury-sider"
+                    collapsedWidth={60}
+                >
+                    <Menu
+                    mode="vertical"
                     onClick={({key})=>{
                         setActiveKey(key)
                     }}
                     defaultSelectedKeys={['home']}
-                    mode="inline"
-                    theme="dark"
+                    theme="light"
                     items={items}
                 />
-            </Sider>
+                </Sider>
 
-                <Content className="mercury-content" style={{height: '100%'}}>
-            <AliveScope>
-                    {
-                        activeKey === 'home' && <KeepAlive>
-                            <Home/>
-                        </KeepAlive>
+                <Content className="mercury-content" style={{ height: '100%' }}>
+                    <AliveScope>
+                        {
+                            activeKey === 'home' && <KeepAlive>
+                                <Home />
+                            </KeepAlive>
 
-                    }
+                        }
+                        {
+                            activeKey === 'accounting' && <KeepAlive>
+                                <Accounting />
+                            </KeepAlive>
+                        }
+
+                        {
+                            activeKey === 'setting' && <KeepAlive>
+                                <Setting />
+                            </KeepAlive>
+                        }
+                    </AliveScope>
                     {
-                        activeKey === 'accounting' && <KeepAlive>
-                            <Accounting/>
-                        </KeepAlive>
-                    }
-                   
-                    {
-                        activeKey === 'setting' && <KeepAlive>
-                            <Setting/>
-                        </KeepAlive>
-                    }
-            </AliveScope> 
-            {
                         activeKey === 'upload' && <KeepAlive>
-                            <Upload/>
+                            <Upload />
                         </KeepAlive>
-            }
-            </Content>
-        </Layout>
+                    }
+                </Content>
+            </Layout>
+        </ConfigProvider>
     );
 }
 
