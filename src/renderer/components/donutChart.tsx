623f8a858024e51c1c74ff2477492d7b473993ec
baiqiu.wsn <baiqiu.wsn@raycloud.com>
2025-04-09 12:11:29 +0800

    优化视图

--- src/renderer/components/donutChart.tsx
+++ src/renderer/components/donutChart.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from "react";
-import { DataView } from "@antv/data-set";
-import { Chart } from "@antv/g2";
-import { formatMoney } from "./utils";
+import React, { useEffect, useRef } from 'react';
+import { DataView } from '@antv/data-set';
+import { Chart } from '@antv/g2';
+import { formatMoney } from './utils';
 interface DataItem {
   value: number;
   type: string;
@@ -23,10 +23,10 @@ const PieChart: React.FC<PieChartProps> = ({ data, height = 500 }) => {
     // 通过 DataSet 计算百分比
     const dv = new DataView();
     dv.source(data).transform({
-      type: "percent",
-      field: "value",
-      dimension: "type",
-      as: "percent",
+      type: 'percent',
+      field: 'value',
+      dimension: 'type',
+      as: 'percent',
     });
 
     const chart = new Chart({
@@ -39,23 +39,23 @@ const PieChart: React.FC<PieChartProps> = ({ data, height = 500 }) => {
     chart.data(dv.rows);
     chart.scale({
       percent: {
-        formatter: (val) => {
-          val = (val * 100).toFixed(2) + "%";
+        formatter: val => {
+          val = (val * 100).toFixed(2) + '%';
           return val;
         },
       },
     });
-    chart.coordinate("theta", {
+    chart.coordinate('theta', {
       radius: 0.5,
     });
     chart.tooltip({
       // showTitle: false,
       showMarkers: false,
-      customItems: (items) => {
-        return items.map((item) => {
+      customItems: items => {
+        return items.map(item => {
           return {
             ...item,
-            name: `${formatMoney(item.data.value, "万")}`,
+            name: `${formatMoney(item.data.value, '万')}`,
             value: `${Number(item.data.percent * 100).toFixed(1)}%`,
           };
         });
@@ -64,18 +64,18 @@ const PieChart: React.FC<PieChartProps> = ({ data, height = 500 }) => {
     chart.legend(false);
     chart
       .interval()
-      .adjust("stack")
-      .position("percent")
-      .color("type")
-      .label("type", {
+      .adjust('stack')
+      .position('percent')
+      .color('type')
+      .label('type', {
         offset: -10,
-        content: (data) => {
-          if (data.percent < 0.05) return "";
-          return `${data.type}`;
+        content: data => {
+          if (data.percent < 0.05) return '';
+          return `${data.type}\n${(data.percent * 100).toFixed(1)}%`;
         },
       })
-      .tooltip("type*percent", (item, percent) => {
-        percent = (percent * 100).toFixed(2) + "%";
+      .tooltip('type*percent', (item, percent) => {
+        percent = (percent * 100).toFixed(2) + '%';
         return {
           name: item,
           value: percent,
@@ -83,39 +83,44 @@ const PieChart: React.FC<PieChartProps> = ({ data, height = 500 }) => {
       })
       .style({
         lineWidth: 1,
-        stroke: "#fff",
+        stroke: '#fff',
       });
 
     const outterView = chart.createView();
     const dv1 = new DataView();
     dv1.source(data).transform({
-      type: "percent",
-      field: "value",
-      dimension: "name",
-      as: "percent",
+      type: 'percent',
+      field: 'value',
+      dimension: 'name',
+      as: 'percent',
     });
 
     outterView.data(dv1.rows);
     outterView.scale({
       percent: {
-        formatter: (val) => {
-          val = (val * 100).toFixed(2) + "%";
+        formatter: val => {
+          val = (val * 100).toFixed(2) + '%';
           return val;
         },
       },
     });
-    outterView.coordinate("theta", {
+    outterView.coordinate('theta', {
       innerRadius: 0.5 / 0.75,
       radius: 0.75,
     });
     outterView
       .interval()
-      .adjust("stack")
-      .position("percent")
-      .color("name", ["#BAE7FF", "#7FC9FE", "#71E3E3", "#ABF5F5", "#8EE0A1", "#BAF5C4"])
-      .label("name")
-      .tooltip("name*percent", (item, percent) => {
-        percent = (percent * 100).toFixed(2) + "%";
+      .adjust('stack')
+      .position('percent')
+      .color('name', ['#BAE7FF', '#7FC9FE', '#71E3E3', '#ABF5F5', '#8EE0A1', '#BAF5C4'])
+      .label('name', {
+        content: data => {
+          if (data.percent < 0.02) return '';
+          return `${data.name}\n${(data.percent * 100).toFixed(1)}%`;
+        },
+      })
+      .tooltip('name*percent', (item, percent) => {
+        percent = (percent * 100).toFixed(2) + '%';
         return {
           name: item,
           value: percent,
@@ -123,10 +128,10 @@ const PieChart: React.FC<PieChartProps> = ({ data, height = 500 }) => {
       })
       .style({
         lineWidth: 1,
-        stroke: "#fff",
+        stroke: '#fff',
       });
 
-    chart.interaction("element-highlight");
+    chart.interaction('element-highlight');
     chart.render();
 
     // 保存 chart 实例以便清理
